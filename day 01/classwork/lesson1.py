c79eed6ac04ee3188475effe20a4adf62cf5e531
nitakadeishvili <nitakado4@gmail.com>
2025-02-09 22:24:09 +0400

    first commit

---
+++ day 01/classwork/lesson1.py
@@ -0,0 +1,14 @@
+name = "Nita"
+surname= "Kadeishvili"
+age = 15
+height = 170.1
+knows_languages= True
+knows_how_to_fly= False
+
+print(name + "" + surname)
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(height))
+print(type(knows_languages))
+print(type(knows_how_to_fly))
---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,10 @@
+name = "Nita"
+surname= "Kadeishvili"
+age = 15
+height = 170
+knows_languages= True
+knows_how_to_fly= False
+
+
+
+print(name + " " + surname + " " + str(age) + " " + str(height)+  " " + str(knows_languages)+ " " + str(knows_how_to_fly))
