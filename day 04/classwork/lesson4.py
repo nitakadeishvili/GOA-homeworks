322ad4a06f16442ed1866fd84bbf03c6b46c94d8
nitakadeishvili <nitakado4@gmail.com>
2025-02-20 11:40:02 +0400

    commit

---
+++ day 04/classwork/lesson4.py
@@ -0,0 +1,32 @@
+fullname = "nita kadeishvili"
+hobby = "cooking"
+print(fullname)
+print(hobby)
+print("fullname")
+
+print(5+9)
+print(5)
+
+
+
+print(5+8)
+print(10-7)
+print(48/4)
+print(90*2)
+
+answer1=13
+answer2=3
+answer3=12.0
+answer4=180
+
+print=(answer1)
+print=(answer2)
+print=(answer3)
+print=(answer4)
+
+level=0
+level=2
+level=level + 1
+level=level + 1
+level=level + 1
+print(level)
---
+++ day 04/homework/lessson4.py
@@ -0,0 +1,25 @@
+Mother= "Nino"
+Father= "Vaxtang"
+Aunt= "Maia"
+Uncle= "Giorgi"
+Sister= "Natia"
+Brother= "David"
+print("my Mother is" + " " + Mother)
+print("my Father is" + " " + Father)
+print("my Aunt is" + " " + Aunt)
+print("my Uncle is" + " " + Uncle)
+print("my Sister is" + " " + Sister)
+print("my Brother is" + " " + Brother)
+
+
+
+number1=7
+number2=10
+number3=3
+number4=18
+number5=5
+
+print(number1 + number2)
+print(number3 * number4)
+print(number4 - number5)
+print(number2 / number5)
