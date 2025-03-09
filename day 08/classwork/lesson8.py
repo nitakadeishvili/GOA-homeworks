c40437d169cbfa51b125890793f061d823cca0fc
nitakadeishvili <nitakado4@gmail.com>
2025-03-09 23:35:36 +0400

    commit

---
+++ day 08/classwork/lesson8.py
@@ -0,0 +1,14 @@
+Name = input ("What's your name?")
+Last_name = input ("What's your last name?")
+print (Name + " " + Last_name)
+
+
+
+Num1 = input ("enter number")
+num2 = input ("enter second number")
+print(Num1 + "+" + num2)
+print(Num1 + num2)
+
+
+Level = input ("your level")
+print(Level + 3)
