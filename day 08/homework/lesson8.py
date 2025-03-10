6750c2d5611798374a3344cb5981245c58c154d9
nitakadeishvili <nitakado4@gmail.com>
2025-03-10 12:54:29 +0400

    commit

---
+++ day 08/homework/lesson8.py
@@ -0,0 +1,66 @@
+#შექმენით 5 ცვლადი რომლებშიც შეინახავთ წიგნების თავდაპირველ ფასს, შემდეგ შექმენით ცვლადი რომელშიც შეინახავთ ფასდაკლების ოდენობას, შექმენით ახალი ფასის მქონე წიგნების ცვლადები, რომელთა მნიშვნელობა იქნება ძველ მნიშვნელობას გამოკლებული ახალი, საბოლოოდ კი დაბეჭდეთ ახალი წიგნების ფასები (გამოიყენეთ კარგი მიდგომები: რომ ცვლადის მნიშვნელობის მინიჭებისას შეგიძლიათ სხვა ცვლადის გამოყენება, კოდი ახსენით კომენტარების საშვალებით, ცვლადებს დაარქვით აღმწერი სახელები snake_case-ის სტილში)
+
+book_price=4
+book1_price=6
+book2_price=8
+book3_price=10
+book4_price=12
+sale=2
+print(book_price / sale)
+print(book1_price / sale)
+print(book2_price / sale)
+print(book3_price / sale)
+print(book4_price / sale)
+newbook_price=2
+newbook1_price=3
+newbook2_price=4
+newbook3_price=5
+newbook4_price=6
+print(newbook_price)
+print(newbook1_price)
+print(newbook2_price)
+print(newbook3_price)
+print(newbook4_price)
+
+
+Name=input("Whats you're name?")
+Surname=input("Your surname ")
+Age=input("How old are you?")
+Location=input("Your location ")
+Occupation=input("Your profession ")
+Hobby=input("Whats your hobby?")
+print(Name + " " + Surname + " " + "is" + " " + Age + " " + "Years old." + " " + Name + " " + "is" + " " + Occupation + " " + "and enjoys" + " " + Hobby)
+
+
+
+# 1. Integer (int) – მთელი რიცხვი შეიცავს მხოლოდ მთელ რიცხვებს (დადებითი, უარყოფითი ან ნული). არ აქვს ათწილადი ნაწილი.არანაირი შეზღუდვა სიდიდეზე (Python-ში int შეუძლია იყოს ძალიან დიდი)
+#2. Float – ათწილადი (მცურავი წერტილი)შეიცავს ათწილად რიცხვებს (რიცხვები წერტილით).იყენებს IEEE 754 სტანდარტს, რაც ნიშნავს, რომ აქვს შეზღუდული სიზუსტე.შეიძლება იყოს ძალიან დიდი ან ძალიან პატარა რიცხვი, მაგრამ ყოველთვის მცურავი წერტილის ფორმატში.
+#integer (int) - მთელი რიცხვებისთვის
+#float - ათწილადებისთვის
+
+
+
+
+
+
+print(5+9)
+print(10-5)
+print(8/2) 
+print(9*3)
+print(8**2)
+print(20//4)
+print(9/2)
+print(27-6)
+print(67/2)
+print(48+3)
+print(33//2)
+print(5**2)
+print(7/2)
+print(6+2)
+print(8//4)
+print(2**5)
+print(6//3)
+print(10*2)
+print(33/2)
+
+
