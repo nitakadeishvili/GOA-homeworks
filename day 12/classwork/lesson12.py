510041e3c00d8b881328bbd2f1a7f87fe396a27e
nitakadeishvili <nitakado4@gmail.com>
2025-03-24 12:55:39 +0400

    commit

---
+++ day 12/classwork/lesson12.py
@@ -0,0 +1,32 @@
+#  1) დაბეჭდეთ 2 რიცხვს შორის (რიცხვები შეცვალეთ) ყველა შესაძლო შედარების ოპერაცია გვერდზე კომენტარის სახით მიუწერეთ პასუხი
+
+print(30>40) #false
+print(60<65)#true
+print(5 == 5) #true
+print(17 <= 20) #true
+print(12 >= 8) #true
+
+#20 სამივე ლოგიკურ ოპერატორზე: not, or, and: ჩამოწერეთ ყველა შესაძლო ვარიანტი და დაბეჭდეთ შედეგი გვერდზე კომენტარის საშვალებით მიწუერეთ შედეგი
+
+print(True and False) #False
+print(False and True) #False
+print(True and True) #True
+print(False and False) #False
+
+print(True or False) #true
+print(False or False) #false
+print(True or True) #true
+print(False or True) #true
+
+print(not True)
+print(not False) #true
+
+
+
+
+
+
+
+
+
+
