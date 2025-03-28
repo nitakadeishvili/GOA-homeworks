ee18ce821016884b5cf10783e876e4e466d44776
nitakadeishvili <nitakado4@gmail.com>
2025-03-29 00:51:51 +0400

    commit

--- day 14/homework/lesson14.py
+++ day 14/homework/lesson14.py
@@ -18,8 +18,26 @@ for number31 in range (31):
 for number80 in range (15,80):
     print(number80)
 #8) for ციკლის საშვალებით გამოიტანეთ რიცხვები 0-იდან 64-ის ჩათვლით
-for number64 in range (65):
+
+for number64 in range (0,65):
     print(number64)
 
+#9) for ციკლის საშვალებით გამოიტანეთ ყოველი მესამე რიცხვი 0-იდან 50-ჩათვლით
 
+for number50 in range (0,50,3):
+    print(number50)
+
+#10) პროგრამას ჩადურად დაათვლევინეთ 50-ის ჩათვლით
+
+for numberchad in range (51):
+    print(numberchad)
+#11) პროგრამას გამოტანინეთ ყველა ლუწი რიცხვი 100-მდე
+
+for numbereven in range(0, 101, 2):
+    print(numbereven)
+
+#პროგრამას გამოატანინეთ რიცხვები 20-იდან 0-მდე, for ციკლით
+
+for number20 in range(20,0,-1):
+    print(number20)
 
